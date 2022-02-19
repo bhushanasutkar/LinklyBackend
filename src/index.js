e94753f4d759f5a93041bd672cfdf4fd18c0b4c7
Pranav <dprnav@gmail.com>
2022-02-19 18:29:21 +0530

    somem deplyoyment fixes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const logger = require('./config/logger');
 
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  logger.info(`Enviroment: ${config.env}`);
   logger.info('Connected to MongoDB');
   server = app.listen(config.port, () => {
     logger.info(`Listening to port ${config.port}`);
